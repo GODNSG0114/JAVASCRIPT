a4efeaa91e0dfed74ef64307e7c75a7260c7814a
Nikhil Suresh God <164666663+GODNSG0114@users.noreply.github.com>
2025-05-12 05:04:16 +0000

    variables

---
+++ 01_basic/01_Variables.js
@@ -0,0 +1,8 @@
+const pi = 381988;
+var accountEmail = "nikhil@gmail.com";
+let accountId = "1234jj";
+city = "vita"
+accountEmail = "new nikhil"
+accountId = 1234443;
+console.log(typeof(accountId));
+console.table([pi , accountEmail , accountId , typeof(accountId) , city ,typeof(city)]);
