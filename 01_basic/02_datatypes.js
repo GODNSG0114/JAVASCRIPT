b2747dbc45acce10f2e85d6551b4355fcc560b96
Nikhil Suresh God <164666663+GODNSG0114@users.noreply.github.com>
2025-05-12 05:04:39 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"   // treate al js code as newer version
+ 
+// alert(3+3)     // not run on nodejs
+
+// data types
+ // number
+ // bigint
+ // string      
+ // boolean
+ // null -> standalone value  ,  typeof (NULL) = object
+ // undefined => 
+ // symbol
+
+ // object     typeof (object) = function;
+ 
+ console.log(typeof(Object));
