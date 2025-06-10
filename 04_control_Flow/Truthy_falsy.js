a92f435de85e7e7607ae505496dc68e9b0358418
nikhil <nikhilgod0114@gmail.com>
2025-06-10 19:07:39 +0530

    control_flow and truthy, falsy values

---
+++ 04_control_Flow/Truthy_falsy.js
@@ -0,0 +1,10 @@
+const userEmail = "nikhil@gmail.com"
+if(userEmail){
+    console.log("Got user email")
+}else{
+    console.log("Don't have user email")
+}
+ // userEmail is considered as true value . this is called truthy
+
+//  falsy vaules => false , 0 , -0 , BigInt 0n , "" , null , undefine , NaN
+//  truthy values => "0" , "false" , " ", [] , {} , function(){}
---
+++ 04_control_Flow/one.js
@@ -0,0 +1,30 @@
+// // if 
+// if(condition){
+
+// }
+
+// // switch 
+//   switch(key){
+//     case val1:
+
+//         break;
+//     case val2:
+
+//         break;
+
+//     default :
+
+//        break;
+//   }
+
+  // nullish coalescing operator (??) : null undefined
+  let val1 ;
+  val1 = 5 ?? 10
+
+  val2 = null ?? 10
+
+  val3 = undefined ?? 15
+
+  console.log(val1) 
+  console.log(val2) 
+  console.log(val3) 
