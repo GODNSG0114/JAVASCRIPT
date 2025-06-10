663093a844b12b355bdec5928a5973aabdf9df6d
nikhil <nikhilgod0114@gmail.com>
2025-06-10 20:22:26 +0530

    loops

---
+++ 05_iterations(Loops)/loops.js
@@ -0,0 +1,17 @@
+// for
+for (let index = 0; index <=10; index++) {
+    console.log(index);
+    
+}
+
+// while 
+while( true) {
+     // code 
+     break;
+}
+ 
+// do.... while
+do{
+
+}
+while(1)
